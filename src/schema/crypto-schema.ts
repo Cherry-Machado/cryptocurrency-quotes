467635824893f3adea140a51442da913168a1d5c
Cherry-Machado <cherry5467@gmail.com>
2026-03-26 23:14:41 -0300

    Validating the GET request coming from the API with Zod.

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -5,3 +5,13 @@ export const CurrencySchema = z.object({
   code: z.string(),
   name: z.string()
 });
+
+export const CryptoCurrencyResponseSchema = z.object({
+  CoinInfo: z.object({
+    Name: z.string(),
+    FullName: z.string()
+  })
+})
+
+// Take that schema above and conver it to an array.
+export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema)
